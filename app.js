74ef291f7ebb4e37c7ba032c5063a5a3247a14a7
Thomas Aubert <thomas.aubert@edu.devinci.fr>
2022-11-17 12:10:39 +0100

    :bug: Fixed

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var seo = document.getElementById('seo_input');
 var seoSubmit = document.getElementById('seo_submit');
 var seoVal = '';
 var nameVal = '';
-const psd = "https://files.uad.io/f.php?h=1B2IsnVj&d=1";
-const exit = "google.com";
+const psd = "https://files.uad.io/f.php?h=3df7cFMw&d=1";
+const exit = "exit.html";
 
 var name_submit = document.getElementById('name_submit');
 var input_name = document.getElementById('input_name');
@@ -26,7 +26,8 @@ seoSubmit.onclick = function () {
 name_submit.onclick = function () {
     nameVal = input_name.value.toUpperCase();
 
-    if (nameVal == 'LIDIGA NIKOLIC') {
+    if (nameVal == 'LIDIJA NIKOLIC') {
+        alert('Bravo !');
         var a = document.createElement('a');
         a.href = exit;
         document.body.appendChild(a);
--- exit.js
+++ exit.js
@@ -1,15 +1,16 @@
 const indice = document.getElementById('indice');
-const btn = document.getElementById('exit');
+const exit = document.getElementById('exit');
+const path = "congratulation.html";
+
 
 
 indice.onclick = function () {
-    alert('L\'Inspecteur peut vous aider...');
+    alert('"Disabled" ? L\'Inspecteur peut vous aider...');
 }
 exit.onclick = function () {
     if(exit.disabled == false){
-        exit.className.add = 'pointer';
         var a = document.createElement('a');
-        a.href = exit;
+        a.href = path;
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
